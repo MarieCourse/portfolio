82526e9ab9a5d484fef7b4d86b8e75ce9a249787
Maria Carrera <meriscarrera@gmail.com>
2023-09-22 15:24:57 +0200

    correction lien linkedin

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -17,7 +17,7 @@ function Contact() {
       />
       <div className="contactMe__icons">
         <NavLink
-          to="https://www.linkedin.com/in/maria-carrera-france/"
+          to="https://www.linkedin.com/in/maria-carrera/"
           target="_blank"
         >
           <FaLinkedinIn className="icon" />
